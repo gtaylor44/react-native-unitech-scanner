223f24fa4bfc955ed00f4c9229c9c0cbe10cb9c2
Greg Taylor <greg.taylor@ignitionls.com>
2025-04-14 08:54:19 +0930

    Fix bug 'java.lang.IllegalArgumentException: Receiver not registered: com.unitech.scanner.BarcodeBroadcastReceiver'

--- android/src/main/java/com/unitech/scanner/ScannerManager.java
+++ android/src/main/java/com/unitech/scanner/ScannerManager.java
@@ -3,95 +3,119 @@ package com.unitech.scanner;
 import android.content.IntentFilter;
 import android.device.ScanManager;
 import android.util.Log;
-
 import com.facebook.react.bridge.LifecycleEventListener;
 import com.facebook.react.bridge.ReactApplicationContext;
 
 public class ScannerManager {
-  private ScanManager scanManager;
-  private ReactApplicationContext reactContext;
-  private static final String TAG = "UTScannerManager";
-  private BarcodeBroadcastReceiver barcodeBroadcastReceiver;
-  private IntentFilter intentFilter = new IntentFilter(ScanManager.ACTION_DECODE);
+    private static final String TAG = "UTScannerManager";
 
-  private final LifecycleEventListener lifecycleEventListener = new LifecycleEventListener() {
-    @Override
-    public void onHostResume() {
-      Log.d(TAG, "onHostResume");
-      reactContext.registerReceiver(barcodeBroadcastReceiver, intentFilter);
+    private ScanManager scanManager;
+    private ReactApplicationContext reactContext;
+    private BarcodeBroadcastReceiver barcodeBroadcastReceiver;
+    private IntentFilter intentFilter = new IntentFilter(ScanManager.ACTION_DECODE);
+
+    private boolean isReceiverRegistered = false;
+
+    private final LifecycleEventListener lifecycleEventListener = new LifecycleEventListener() {
+        @Override
+        public void onHostResume() {
+            Log.d(TAG, "onHostResume");
+            // No need to re-register, it's already registered once
+        }
+
+        @Override
+        public void onHostPause() {
+            Log.d(TAG, "onHostPause");
+            // No need to unregister during pause
+        }
+
+        @Override
+        public void onHostDestroy() {
+            Log.d(TAG, "onHostDestroy");
+            unregisterReceiverSafely();
+        }
+    };
+
+    public ScannerManager(ReactApplicationContext reactContext) {
+        this.reactContext = reactContext;
+        this.scanManager = new ScanManager();
+        this.barcodeBroadcastReceiver = new BarcodeBroadcastReceiver(reactContext);
+
+        registerReceiverSafely();
+        this.reactContext.addLifecycleEventListener(lifecycleEventListener);
     }
 
-    @Override
-    public void onHostPause() {
-      Log.d(TAG, "onHostPause");
-      reactContext.unregisterReceiver(barcodeBroadcastReceiver);
+    // SAFELY register the receiver
+    private void registerReceiverSafely() {
+        if (!isReceiverRegistered) {
+            reactContext.registerReceiver(barcodeBroadcastReceiver, intentFilter);
+            isReceiverRegistered = true;
+            Log.d(TAG, "Barcode receiver registered");
+        }
     }
 
-    @Override
-    public void onHostDestroy() {
-      Log.d(TAG, "onHostDestroy");
-      reactContext.unregisterReceiver(barcodeBroadcastReceiver);
-    }
-  };
-
-  public ScannerManager(ReactApplicationContext reactContext) {
-    this.reactContext = reactContext;
-    this.scanManager = new ScanManager();
-    this.barcodeBroadcastReceiver = new BarcodeBroadcastReceiver(reactContext);
-    this.reactContext.registerReceiver(this.barcodeBroadcastReceiver, this.intentFilter);
-    this.reactContext.addLifecycleEventListener(lifecycleEventListener);
-  }
-
-  public boolean getScannerState() {
-    boolean result = scanManager.getScannerState();
-    Log.d(TAG, "getScannerState: " + result);
-    return result;
-  }
-
-  public boolean openScanner() {
-    boolean result = scanManager.openScanner();
-    Log.d(TAG, "openScanner: " + result);
-    return result;
-  }
-
-  public boolean closeScanner() {
-    boolean result = scanManager.closeScanner();
-    Log.d(TAG, "closeScanner: " + result);
-    return result;
-  }
-
-  public boolean startDecode() {
-    boolean isScannerOn = this.getScannerState();
-    if (!isScannerOn) {
-      this.openScanner();
+    // SAFELY unregister the receiver
+    private void unregisterReceiverSafely() {
+        if (isReceiverRegistered) {
+            try {
+                reactContext.unregisterReceiver(barcodeBroadcastReceiver);
+                Log.d(TAG, "Barcode receiver unregistered");
+            } catch (IllegalArgumentException e) {
+                Log.w(TAG, "Barcode receiver was already unregistered", e);
+            }
+            isReceiverRegistered = false;
+        }
     }
 
-    boolean result = scanManager.startDecode();
-    Log.d(TAG, "startDecode: " + result);
-    return result;
-  }
+    // --- Scanner control methods ---
+    public boolean getScannerState() {
+        boolean result = scanManager.getScannerState();
+        Log.d(TAG, "getScannerState: " + result);
+        return result;
+    }
 
-  public boolean stopDecode() {
-    boolean result = scanManager.stopDecode();
-    Log.d(TAG, "stopDecode: " + result);
-    return result;
-  }
+    public boolean openScanner() {
+        boolean result = scanManager.openScanner();
+        Log.d(TAG, "openScanner: " + result);
+        return result;
+    }
 
-  public boolean getTriggerLockState() {
-    boolean result = scanManager.getTriggerLockState();
-    Log.d(TAG, "getTriggerLockState: " + result);
-    return result;
-  }
+    public boolean closeScanner() {
+        boolean result = scanManager.closeScanner();
+        Log.d(TAG, "closeScanner: " + result);
+        return result;
+    }
 
-  public boolean lockTrigger() {
-    boolean result = scanManager.lockTrigger();
-    Log.d(TAG, "lockTrigger: " + result);
-    return result;
-  }
+    public boolean startDecode() {
+        if (!getScannerState()) {
+            openScanner();
+        }
+        boolean result = scanManager.startDecode();
+        Log.d(TAG, "startDecode: " + result);
+        return result;
+    }
 
-  public boolean unlockTrigger() {
-    boolean result = scanManager.unlockTrigger();
-    Log.d(TAG, "unlockTrigger: " + result);
-    return result;
-  }
+    public boolean stopDecode() {
+        boolean result = scanManager.stopDecode();
+        Log.d(TAG, "stopDecode: " + result);
+        return result;
+    }
+
+    public boolean getTriggerLockState() {
+        boolean result = scanManager.getTriggerLockState();
+        Log.d(TAG, "getTriggerLockState: " + result);
+        return result;
+    }
+
+    public boolean lockTrigger() {
+        boolean result = scanManager.lockTrigger();
+        Log.d(TAG, "lockTrigger: " + result);
+        return result;
+    }
+
+    public boolean unlockTrigger() {
+        boolean result = scanManager.unlockTrigger();
+        Log.d(TAG, "unlockTrigger: " + result);
+        return result;
+    }
 }
